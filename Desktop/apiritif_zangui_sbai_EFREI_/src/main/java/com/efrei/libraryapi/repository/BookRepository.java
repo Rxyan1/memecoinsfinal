29cdd594e490344f5a340b1669a187f1f4249df5
Rxyan1 <126580722+Rxyan1@users.noreply.github.com>
2025-06-02 14:16:15 +0200

    Delete Desktop/apiritif_zangui_sbai_EFREI_ directory

--- Desktop/apiritif_zangui_sbai_EFREI_/src/main/java/com/efrei/libraryapi/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.efrei.libraryapi.repository;
-
-import com.efrei.libraryapi.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
